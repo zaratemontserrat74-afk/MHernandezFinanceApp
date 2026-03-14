a02c61f54d4ac1d9d13e776dafcd961c7c3ff127
Montse <zaratemontserrat74@gmail.com>
2026-03-14 15:59:47 -0600

    home

--- app/src/main/java/com/pajasoft/mhernandezfinanceapp/components/Home.kt
+++ app/src/main/java/com/pajasoft/mhernandezfinanceapp/components/Home.kt
@@ -1,2 +1,9 @@
 package com.pajasoft.mhernandezfinanceapp.components
 
+import androidx.compose.runtime.Composable
+import com.pajasoft.mhernandezfinanceapp.models.User
+
+@Composable
+fun Homescreen(){
+    val user = User(nombre = "")
+}
