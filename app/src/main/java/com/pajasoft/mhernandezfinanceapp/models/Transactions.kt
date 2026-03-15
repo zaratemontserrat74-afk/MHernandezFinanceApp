911f37fdccef8402ae77d26a2d538c7c3fd9c3e7
Montse <zaratemontserrat74@gmail.com>
2026-03-14 22:18:48 -0600

    iconos otra vez

--- app/src/main/java/com/pajasoft/mhernandezfinanceapp/models/Transactions.kt
+++ app/src/main/java/com/pajasoft/mhernandezfinanceapp/models/Transactions.kt
@@ -1,8 +1,11 @@
 package com.pajasoft.mhernandezfinanceapp.models
 
+import androidx.compose.ui.graphics.vector.ImageVector
+
 data class Transaction(
     val name:String,
     val category: String,
     val amount: Double,
-    val time: String
+    val time: String,
+    val icon: ImageVector
 )
