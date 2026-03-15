10958fb80efb6dff30b03bd347f55e5f5f3d3aaf
Montse <zaratemontserrat74@gmail.com>
2026-03-14 20:08:56 -0600

    main

--- app/src/main/java/com/pajasoft/mhernandezfinanceapp/MainActivity.kt
+++ app/src/main/java/com/pajasoft/mhernandezfinanceapp/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key.Companion.Home
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.pajasoft.mhernandezfinanceapp.components.Homescreen
 import com.pajasoft.mhernandezfinanceapp.ui.theme.MHernandezFinanceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +25,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MHernandezFinanceAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Homescreen(innerPadding)
                 }
             }
         }
@@ -31,17 +33,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Homescreen(innerPadding: PaddingValues) {
+    Box(
+        modifier = Modifier
+        .padding(innerPadding)){
+            Homescreen()
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MHernandezFinanceAppTheme {
-        Greeting("Android")
+        Homescreen(innerPadding = PaddingValues(top = 0.dp))
     }
 }
